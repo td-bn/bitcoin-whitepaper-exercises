7c0f7c659bdf8c3630dfd5b39e9526ee3f92861b
td-bn <bnabi@protonmail.com>
2021-09-08 16:23:28 +0530

    add finished hashing.js

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { assert } = require("console");
 var crypto = require("crypto");
 
 // The Power of a Smile
@@ -27,17 +28,55 @@ Blockchain.blocks.push({
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	Blockchain.blocks.push(createBlock(line));
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
 
 function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+	const str = JSON.stringify(bl);
+	const buff = Buffer.from(str);
+	assert(buff instanceof Buffer);
+	return crypto.createHash("sha256").update(Buffer.from(str)).digest("hex");
+}
+
+function createBlock(data) {
+	const len = Blockchain.blocks.length;
+	const prevBlock = Blockchain.blocks[len-1];
+	const prevHash = blockHash(prevBlock);
+	const timeStamp = Date.now();
+
+	return {
+		index: len,
+		prevHash: prevHash,
+		data: data,
+		timestamp: timeStamp,
+		hash: crypto.createHash("sha256").update(`${len}${prevHash}${data}${timeStamp}`).digest("hex")
+	};
+}
+
+function verifyBlock(bl) {
+	assert(bl.index>=0);
+	assert(bl.data != null);
+	assert(bl.prevHash != null);
+
+	return true;
+}
+
+function verifyChain(blockchain) {
+	const blocks = blockchain.blocks;
+	assert(blocks[0].hash == "000000");
+
+	let currentBlock, previousBlock = blocks[0];
+	for(let i=1; i<9; i++) {
+		currentBlock = blocks[i];
+		verifyBlock(currentBlock);
+		assert(currentBlock.prevHash = blockHash(previousBlock))
+	}
+	
+	return true;
 }
